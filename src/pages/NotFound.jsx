ab47981c2fd490449361cb6a7b3ba1fe010cc7a6
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-07-27 11:29:19 +0200

    Finished NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -11,7 +12,13 @@ export default function NotFound() {
   return (
     <div>
       <Header />
-      <main className="main"></main>
+      <div className="notfound">
+        <h1 className="notfound-code">404</h1>
+        <p className="notfound-text">Page not found</p>
+        <Link className="notfound-link" to="/">
+          Return to home page
+        </Link>
+      </div>
       <Footer />
     </div>
   );
