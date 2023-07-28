c7378d6b99bdca694ab216386e8a26cc008bfbac
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2023-07-28 11:41:36 +0200

    Added conditional rendering of profile page header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../images/argentBankLogo.png";
 
 /**
@@ -8,17 +8,33 @@ import logo from "../images/argentBankLogo.png";
  * @returns {JSX.Element} - Rendered component.
  */
 export default function Header() {
+  const location = useLocation();
+  const isProfileCurrentPage = location.pathname === "/profile";
+
   return (
     <header className="header container">
       <Link to="/">
         <img className="header-logo" src={logo} alt="Argent Bank Logo" />
       </Link>
-      <div className="header-signin">
-        <i class="header-signin-icon fa-solid fa-user-circle"></i>
-        <Link className="header-signin-link" to="/signin">
-          Sign In
-        </Link>
-      </div>
+      {isProfileCurrentPage ? (
+        <div className="header-signin">
+          <i class="header-signin-icon fa-solid fa-user-circle"></i>
+          <Link className="header-signin-link" to="/profile/edit">
+            UserName
+          </Link>
+          <i class="header-signin-icon fa-solid fa-sign-out"></i>
+          <Link className="header-signin-link" to="/">
+            Sign Out
+          </Link>
+        </div>
+      ) : (
+        <div className="header-signin">
+          <i class="header-signin-icon fa-solid fa-user-circle"></i>
+          <Link className="header-signin-link" to="/signin">
+            Sign In
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
